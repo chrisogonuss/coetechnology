f31e6bb07afc9ae8b38051f096fb782d12d89d15
Mbahvictor16 <mbahvictor16@gmail.com>
2026-01-17 01:25:24 +0100

    Icon and Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,12 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'COE HUB Technology',
     description: 'Web dev Agency',
-    images: ["./coelogo.png"],
+    images: {
+      url: './coelogo.png',
+      width: 1200,
+      height: 630,
+      alt: 'COE HUB Technology Logo',
+    }
   }
 }
 
